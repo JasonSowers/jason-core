6d413a7ebd7b97042f98b053523bad8c68ae0a2f
Jason Sowers <v-jassow@microsoft.com>
2017-12-05 12:25:32 -0800

    core2.0 commit

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,63 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Http;
-
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Connector;
-using Microsoft.Bot.Builder.Dialogs;
-using System.Web.Http.Description;
-using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 
-namespace Microsoft.Bot.Sample.SimpleEchoBot
+namespace Core2._0.Controllers
 {
-    [BotAuthentication]
-    public class MessagesController : ApiController
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
     {
-        /// <summary>
-        /// POST: api/Messages
-        /// receive a message from a user and send replies
-        /// </summary>
-        /// <param name="activity"></param>
-        [ResponseType(typeof(void))]
-        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        //IConfiguarationRoot needs to be used here instead of IConfiguration like in .NET Core 2.0
+        private readonly IConfiguration configuration;
+        public MessagesController(IConfiguration configuration)
         {
-            // check if activity is of type message
-            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            this.configuration = configuration;
+        }
+
+        [Authorize(Roles = "Bot")]
+        [HttpPost]
+        public async Task<OkResult> Post([FromBody] Activity activity)
+        {
+            var appCredentials = new MicrosoftAppCredentials(this.configuration);
+            var client = new ConnectorClient(new Uri(activity.ServiceUrl), appCredentials);
+            var reply = activity.CreateReply();
+            if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new EchoDialog());
+                reply.Text = $"echo: {activity.Text}";
             }
             else
             {
-                HandleSystemMessage(activity);
+                reply.Text = $"activity type: {activity.Type}";
             }
-            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
-        }
-
-        private Activity HandleSystemMessage(Activity message)
-        {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
-
-            return null;
+            await client.Conversations.ReplyToActivityAsync(reply);
+            return Ok();
         }
     }
 }
